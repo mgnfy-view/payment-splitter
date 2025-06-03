dc3f60a7253ef67547a7e96dcc351ae2917fd653
Sahil-Gujrati <sahilgujrati12@gmail.com>
2025-06-03 15:35:15 +0530

    feat: Add deployment script

--- script/DeployPaymentSplitter.s.sol
+++ script/DeployPaymentSplitter.s.sol
@@ -1,16 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Script} from "forge-std-1.9.7/src/Script.sol";
+import { Script } from "forge-std-1.9.7/src/Script.sol";
 
-import {PaymentSplitter} from "@src/PaymentSplitter.sol";
+import { PaymentSplitter } from "@src/PaymentSplitter.sol";
 
 contract DeployPaymentSplitter is Script {
+    address public wrappedNativeToken;
+    address public owner;
+
     PaymentSplitter public paymentSplitter;
+
+    function setUp() public {
+        wrappedNativeToken = address(0x123);
+        owner = address(0x456);
+    }
 
     function run() public returns (PaymentSplitter) {
         vm.startBroadcast();
-        paymentSplitter = new PaymentSplitter();
+        paymentSplitter = new PaymentSplitter(owner, wrappedNativeToken);
         vm.stopBroadcast();
 
         return paymentSplitter;
