d92610b759437d99fa3cfc179548e2aae4412d0b
Sahil-Gujrati <sahilgujrati12@gmail.com>
2025-06-06 16:59:58 +0530

    refactor: Update deployment script

--- script/DeployPaymentSplitter.s.sol
+++ script/DeployPaymentSplitter.s.sol
@@ -8,17 +8,29 @@ import { PaymentSplitter } from "@src/PaymentSplitter.sol";
 contract DeployPaymentSplitter is Script {
     address public wrappedNativeToken;
     address public owner;
+    address[] public tokens;
+    address[] public payees;
+    uint256[] public shares;
 
     PaymentSplitter public paymentSplitter;
 
     function setUp() public {
         wrappedNativeToken = address(0x123);
         owner = address(0x456);
+
+        // dummy values, change on run
+        tokens.push(address(0x321));
+        tokens.push(address(0x321));
+        payees.push(address(0x123));
+        payees.push(address(0x456));
+        shares.push(5000);
+        shares.push(5000);
     }
 
     function run() public returns (PaymentSplitter) {
         vm.startBroadcast();
         paymentSplitter = new PaymentSplitter(owner, wrappedNativeToken);
+        paymentSplitter.setPayees(payees, tokens, shares);
         vm.stopBroadcast();
 
         return paymentSplitter;
